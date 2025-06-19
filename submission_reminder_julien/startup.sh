e4cbfdd99557383391cc328aad5b7b6f8d6cb220
Julien Dusabe <j.dusabe@alustudent.com>
2025-06-19 12:53:23 -0700

    reedit

--- submission_reminder_julien/startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Start reminder script if it exists
-if [ -f "./app/reminder.sh" ]; then
-    echo "Starting the reminder script..."
-    ./app/reminder.sh
-else
-    echo "reminder.sh not found!"
-fi
